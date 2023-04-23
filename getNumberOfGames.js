e0d63f97bd7868d3f25c63cea33f7d589d0744f8
Romanchik00 <116202502+Romanchik00@users.noreply.github.com>
2023-04-23 18:36:27 +0300

    Update getNumberOfGames.js

--- getNumberOfGames.js
+++ getNumberOfGames.js
@@ -1,6 +1,11 @@
-function getNumberOfGames(numberOfCommands) {
+function getNumberOfGames() {
+  let numberOfCommands = prompt("Введите количество команд (от 2 до 99)")
   let gamesForRaund = 0;
   let summOfGame = 0;
+  if (numberOfCommands <= 1 || numberOfCommands >= 100) {
+    alert("От 2 До 100 !!!");
+    return(numberOfCommands);
+  }
     while (numberOfCommands > 1) {
     if (numberOfCommands % 2 == 0) {
       gamesForRaund = numberOfCommands / 2;
@@ -11,5 +16,5 @@ function getNumberOfGames(numberOfCommands) {
       summOfGame += gamesForRaund;
       numberOfCommands = (numberOfCommands - 1) / 2 + 1;
     }
-  } console.log(summOfGame)
+  } alert(summOfGame)
   }
